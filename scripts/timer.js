20489e342fd1df2f29cf6da15c57420e40ef999f
nmelgar <nmelgar16013@gmail.com>
2025-02-20 17:00:38 -0600

    Logic for timer not working yet.

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,1 +1,30 @@
 // include function for timer logic, function to start, stop, pause, restart and update the timer
+
+
+
+setInterval(myTimer, 1000);
+
+function myTimer() {
+    const d = new Date();
+    document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+}
+
+function startTimer() {
+    var seconds = 1500;
+    var minutes = seconds / 60
+    var seconds = 60
+
+
+    var el = document.getElementById('timer');
+
+    function incrementSeconds() {
+        if (seconds = 59) {
+            minutes -= 1
+        }
+        seconds -= 1;
+        // el.innerText = "Timer " + minutes + ":" + seconds;
+        el.innerText = "Timer " + minutes + ":" + seconds;
+    }
+
+    var cancel = setInterval(incrementSeconds, 1000);
+}
